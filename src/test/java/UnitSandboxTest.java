ea6a3fe7fce07300ff06b4954105590a0fc548ca
pefros <parascovia.efros@endava.com>
2025-10-20 11:00:36 +0300

    MDDAP005-32 Update ScenarioContext implementation

--- src/test/java/UnitSandboxTest.java
+++ src/test/java/UnitSandboxTest.java
@@ -1,10 +1,6 @@
-import org.example.utils.PropertyReader;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 /**
  * This is a sandbox to play around with Java core and unit tests.
@@ -17,19 +13,5 @@ public class UnitSandboxTest {
     @Test
     public void exampleUnitTest() {
         log.info("Hello world");
-    }
-
-    @Test
-    void verifyPropertiesLoaded() {
-        PropertyReader propertyReader = PropertyReader.getInstance();
-
-        assertEquals("https://wikipedia.org", propertyReader.getProperty("app.url"));
-        assertEquals("chromium", propertyReader.getProperty("browser.type"));
-        assertEquals(1366, propertyReader.getIntProperty("browser.viewport.size.width"));
-        assertEquals(768, propertyReader.getIntProperty("browser.viewport.size.height"));
-        assertEquals(4500, propertyReader.getIntProperty("browser.slow.mo"));
-        assertFalse(propertyReader.getBooleanProperty("browser.headless"));
-
-        assertEquals(10000, propertyReader.getIntProperty("timeout.default.ms"));
     }
 }
--- src/test/java/org/example/context/ScenarioContext.java
+++ src/test/java/org/example/context/ScenarioContext.java
@@ -1,23 +1,48 @@
 package org.example.context;
 
-import org.apache.commons.lang3.NotImplementedException;
-
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 /**
  * ScenarioContext is a simple storage used to share data between Cucumber step definitions
  * during a single scenario execution.
  */
 public class ScenarioContext {
-    
-    public void save(String key, Object data){
-        throw new NotImplementedException("Method not implemented");
+
+    private static final ScenarioContext INSTANCE = new ScenarioContext();
+    private final Map<String, Object> scenarioContext = new HashMap<>();
+
+    public static ScenarioContext getInstance() {
+        return INSTANCE;
     }
-    
-    public void get(String key){
-        throw new NotImplementedException("Method not implemented");
+
+    public void setContext(String key, Object value) {
+        if (scenarioContext.containsKey(key)) {
+            throw new IllegalStateException("Key already exists: " + key);
+        }
+        scenarioContext.put(key, value);
     }
-    
+
+    public void scenarioUpdate(String key, Object value) {
+        if (!scenarioContext.containsKey(key)) {
+            throw new IllegalStateException("Key does not exist: " + key);
+        }
+        scenarioContext.put(key, value);
+    }
+
+    public Object getValue(String key) {
+        if (!scenarioContext.containsKey(key)) {
+            throw new IllegalStateException("Key does not exist: " + key);
+        }
+        return scenarioContext.get(key);
+    }
+
+    public <T> T getValueAs(String key, Class<T> tClass) {
+        Object value = getValue(key);
+        return tClass.cast(value);
+    }
+
+    public void clearScenarioContext() {
+        scenarioContext.clear();
+    }
 }
